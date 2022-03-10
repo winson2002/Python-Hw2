6037e05d3c3d3c03af5bde750283fa26da588187
winson2002 <99623185+winson2002@users.noreply.github.com>
2022-03-09 20:26:39 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -3,7 +3,8 @@ import time
 def calculate_time(func):
 	def wrapper(func):
 		start = time.time()
-		func()
+		func = func()
 		end = time.time()
 		print(f'Total time {start - end}')
+		return func
 	return wrapper
