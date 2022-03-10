c3127146a151530e86bb74d938214dc3263605f8
Winson Ma <winson.ma@sjsu.edu>
2022-03-09 21:52:11 -0800

    Edit11

--- sort.py
+++ sort.py
@@ -2,10 +2,14 @@ def sort_list(list):
 	n = len(list)
 	i = 0
 	while i < n:
-		j = n-i-1
+		j = i+1
 		while j < n:
 			if list[i] > list[j]:
 				list[i],list[j] = list[j],list[i]
-			j-=1
+			j+=1
 		i+=1
 	return(list)
+
+
+a = [10,9,8,7,6,5,4,3,2,1,11,32,30]
+print(sort_list(a))
