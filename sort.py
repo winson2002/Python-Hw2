668b2a2f1f231a237d3f4104e24296cfbfdd21b1
Winson Ma <winson.ma@sjsu.edu>
2022-03-09 21:18:10 -0800

    Edit8

--- sort.py
+++ sort.py
@@ -3,9 +3,9 @@ def sort_list(list):
 	i = 0
 	while (i < n):
 		j = n-i-1
-		while (j < n):
+		while (j > 0):
 			if (list[i] < list[j]):
 				list[i],list[j] = list[j],list[i]
-			j+=1
+			j-=1
 		i+=1
 	return(list)
